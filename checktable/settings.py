ef636ec5094e213c59bfcbc7ca531117b98f333a
zoonong <zoonongzung@naver.com>
2022-09-27 23:30:30 +0900

    static file 정리

--- checktable/settings.py
+++ checktable/settings.py
@@ -131,9 +131,16 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#STATIC 상속 설정
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'checktable', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
